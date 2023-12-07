7d8d8c7583a07642dd07462549e7c93209933237
luis67 <lu1s030>
2023-12-06 17:51:58 -0800

    TWO NEW FILES! CHEERS!

---
+++ example1.py
@@ -0,0 +1,10 @@
+# Example file 1
+
+a = 1
+b = 1
+c = input(int("What is 1 + 1?\n"))
+
+if c == 2:
+    print("Nice Job!")
+else:
+    print("Try Again...")
---
+++ example2.py
@@ -0,0 +1,5 @@
+# Example file 2
+
+a = input("Enter number:")
+b = input("Enter 2nd number:")
+print(a+b)
